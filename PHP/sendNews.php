a1d5f5b42f799f13488ac0d0807485bc3dfa595e
Riccardo Torrisi <r.torrisi96@gmail.com>
2016-10-31 14:50:25 +0100

    Update sendNews form

--- PHP/sendNews.php
+++ PHP/sendNews.php
@@ -1,20 +1,77 @@
 <?php
 
+$botUrl = "https://api.telegram.org/bot174341771:AAGK6plXw0OW6FeHF3WPiyMtvUzzVau3wNg/";
 $conn = mysqli_connect("localhost", "root", "admin", "FSO_Database") or die("error");
 
 if($_POST){
 
+    $type = $_POST['type'];
     $message = $_POST['message'];
-    $image=addslashes(file_get_contents($_FILES['file_attach']['tmp_name'][0]));
 
-    //$sql = "INSERT INTO Admin (username, nome, cognome, chat_id, password, profile_pic)
-    //VALUES ('$username', '$nome', '$cognome', '$chat_id', '$password', '$image')";
+    $info = pathinfo($_FILES['file_attach']['name'][0]);
+    $ext = $info['extension'];
+    $target = '../img/file.'.$ext;
+    move_uploaded_file($_FILES['file_attach']['tmp_name'][0], $target);
 
-    //if ($conn->query($sql) === TRUE) {
-      print json_encode(array('type'=>'done', 'text' => 'News sended! '.$message));
-    //}
-    //else { print json_encode(array('type'=>'error', 'text' => 'News failed!')); }
 
+    $chatId = "127080847";
+
+      if($type=="text") {
+          $text = urlencode($message);
+
+          $result=file_get_contents($botUrl."sendmessage?chat_id=".$chatId."&text=".$text);
+          //INS QUI INVIO A TUTTI
+      }
+      else if($type=="image") {
+        $img = curl_file_create('../img/file.'.$ext);
+        // FOTO
+        $target_url = $botUrl.'sendPhoto';
+        $post = array(
+            'chat_id'   => $chatId,
+            'photo'  => $img,
+            'caption'   => $message
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$target_url);
+        curl_setopt($ch, CURLOPT_POST,1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        $result=curl_exec ($ch);
+        curl_close ($ch);
+
+        $array = json_decode($result, true);
+        $image_id = $array['result']['photo'][0]['file_id'];
+        $text = urlencode($message);
+        file_get_contents($botUrl.'sendPhoto?chat_id='.$chatId.'&photo='.$image_id.'&caption='.$text);
+
+      }
+      else {
+        $file = curl_file_create('../img/file.'.$ext);
+        //FILE
+        $target_url = $botUrl.'sendDocument';
+        $post = array(
+            'chat_id'   => $chatId,
+            'document'  => $file,
+            'caption'   => $message
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$target_url);
+        curl_setopt($ch, CURLOPT_POST,1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        $result=curl_exec ($ch);
+        curl_close ($ch);
+
+        $array = json_decode($result, true);
+        $document_id = $array['result']['document']['file_id'];
+        $text = urlencode($message);
+        file_get_contents($botUrl.'sendDocument?chat_id='.$chatId.'&document='.$document_id.'&caption='.$text);
+      }
+
+      if(strlen($result)>0) print json_encode(array('type'=>'done', 'text' => 'News sended!'));
+      else { print json_encode(array('type'=>'error', 'text' => 'News failed!')); }
 }
 
 $conn->close();
